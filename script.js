4a8b70f87b4d52188c55a596d2d0c682f45dbdd8
Narasapu vishnu vardhan <nagarjunavishnu16@gmail.com>
2025-07-08 10:01:26 +0530

    Committed

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-document.getElementById('loginform').addEventListener('submit',async function(e){
-    e.preventDefault();
-    const jobtitle = document.getElementById("jobtitle").value;
-    const location = document.getElementById("location").value;
-    const experience = document.getElementById("Experience").value;
+// document.getElementById('loginform').addEventListener('submit',async function(e){
+//     e.preventDefault();
+//     const jobtitle = document.getElementById("jobtitle").value;
+//     const location = document.getElementById("location").value;
+//     const experience = document.getElementById("Experience").value;
 
-    const response = await fetch('http://localhost:3000/searchjobs',{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json',
-        },
-        body:JSON.stringify({jobtitle,location,experience})
-    });
-    const res = await response.json();
-    alert(res.message);
-});
+//     const response = await fetch('http://localhost:3000/searchjobs',{
+//         method:'POST',
+//         headers:{
+//             'Content-Type':'application/json',
+//         },
+//         body:JSON.stringify({jobtitle,location,experience})
+//     });
+//     const res = await response.json();
+//     alert(res.message);
+// });
+
+
+
+// var a = document.getElementById('jobtitle');
+// console.log(a);
+var a = document.getElementById('search-jobs');
+console.log(a);
+ a.addEventListener('click',function(e){
+     e.preventDefault();
+     
+    alert("clicked");
+   
+})
+
+// var b = document.getElementById('location');
+// console.log(b);
+
+// var c = document.getElementById('Experience');
+// console.log(c);
+   
+// const b =a.addEventListener('submit',async function(){
+// console.log(b);
+   
+
+// })    // alert("Button is clicked");
 
--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const {Pool} = require('pg');
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const {Pool} = require('pg');
 
-const app = express();
-const Port = 3000;
+// const app = express();
+// const Port = 3000;
 
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
-const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    password:'1234567890',
-    database:'example',
-    port:5432
-});
+// const pool = new Pool({
+//     user:'postgres',
+//     host:'localhost',
+//     password:'1234567890',
+//     database:'example',
+//     port:5432
+// });
 
-app.post('/searchjobs',async(req,res)=>{
-const {jobtitle,location,experience} = req.body;
-    try{
-        await pool.query(
-            'INSERT INTO jobsearch(jobtitle,location,experience)Values($1,$2,$3)',
-            [jobtitle,location,experience],
-        );
+// app.post('/searchjobs',async(req,res)=>{
+// const {jobtitle,location,experience} = req.body;
+//     try{
+//         await pool.query(
+//             'INSERT INTO jobsearch(jobtitle,location,experience)Values($1,$2,$3)',
+//             [jobtitle,location,experience],
+//         );
        
-        res.json({message:'user saved successfully'})
-    }
-    catch(err)
-    {
-        console.error(err);
-        res.status(500).json({message:'Error is Avoiding to store in the database'});
-    }
-});
+//         res.json({message:'user saved successfully'})
+//     }
+//     catch(err)
+//     {
+//         console.error(err);
+//         res.status(500).json({message:'Error is Avoiding to store in the database'});
+//     }
+// });
 
-// const Port = process.env.Port||5000;
-app.listen(Port,()=>{
-    console.log('connected to db')
-    console.log(`Server running on:${Port}`);
-});
+// // const Port = process.env.Port||5000;
+// app.listen(Port,()=>{
+//     console.log('connected to db')
+//     console.log(`Server running on:${Port}`);
+// });
